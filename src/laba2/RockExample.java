df5e8ec76a8877b2976372316bd20b5e33d423ec
teterchuk <chelakina@bk.ru>
2021-06-04 19:24:53 +0900

    Result

--- src/laba2/RockExample.java
+++ src/laba2/RockExample.java
@@ -5,7 +5,6 @@ public class RockExample {
     private int maxPerepad;
     private int lengthRoute;
 
-
     public int getMaxVersh() {
         return maxVersh;
     }
@@ -29,6 +28,7 @@ public class RockExample {
     public void setLengthRoute(int lengthRoute) {
         if (lengthRoute > 2) {
             this.lengthRoute = lengthRoute;
+
         } else {
             System.out.println("Длина пути не может быть меньше трех");
         }
@@ -41,7 +41,34 @@ public class RockExample {
         int max = 1;
         int min = 0;
 
-        System.out.print(route[0] + " ");
+        System.out.print(route[0] + " " + route[1] + " ");
+
+        int dif = 1; //max - min
+
+        for (int i = 2; i < lengthRoute; i++) {
+
+            int a = down(min, maxPerepad - dif);
+            int b = up(max, maxPerepad - dif);
+
+
+            route[i] = Math.round((float)Math.random() * (b - a) + a);
+
+            //Ищем мин. и максимум двух соседних вершин
+            if (route[i-1] > route[i]) {
+                max = route[i-1];
+                min = route[i];
+            }
+            else {
+                max = route[i];
+                min = route[i-1];
+            }
+
+            dif = Math.abs(max-min);
+
+            System.out.print(route[i] + " ");
+
+        }
+
 
     }
 
--- src/laba2/main.java
+++ src/laba2/main.java
@@ -2,6 +2,12 @@ package laba2;
 
 public class main {
     public static void main(String[] args) {
+        RockExample test = new RockExample();
+        test.setLengthRoute(10);
+        test.setMaxPerepad(7);
+        test.setMaxVersh(30);
+
+        test.getRoute();
 
     }
 }
